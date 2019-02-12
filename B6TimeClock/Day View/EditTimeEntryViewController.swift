54b294b05d31fdf32de7be834290b55e7c3d1c48
Gary Robinson <garyisscary@gmail.com>
2019-02-12 09:08:38 -0700

    progress on editing time entries

--- B6TimeClock/Day View/EditTimeEntryViewController.swift
+++ B6TimeClock/Day View/EditTimeEntryViewController.swift
@@ -15,6 +15,11 @@ class EditTimeEntryViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var deleteButton: UIButton!
 
     var entry: TimeEntry?
+    var toolbar = UIToolbar()
+
+    //TODO: make my own picker view that allows seconds
+    var startTimePicker = UIDatePicker()
+    var durationTimePicker = UIDatePicker()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +29,7 @@ class EditTimeEntryViewController: UIViewController, UITextFieldDelegate {
             return
         }
 
-        //TODO: use date/time pickers as the input views, add a toolbar & done button on them
-        startTextField.text = timeEntry.startTime.formattedTime()
-        durationTextField.text = Date.formattedDuration(stopTime.timeIntervalSince(timeEntry.startTime))
+        initTextFields(entry: timeEntry, stopTime: stopTime)
 
         let saveButton = UIBarButtonItem(barButtonSystemItem: .save, target: self,
                                          action: #selector(EditTimeEntryViewController.saveTapped))
@@ -34,9 +37,102 @@ class EditTimeEntryViewController: UIViewController, UITextFieldDelegate {
         navigationItem.rightBarButtonItem = saveButton
     }
 
+    func initTextFields(entry: TimeEntry, stopTime: Date) {
+        toolbar = UIToolbar()
+        toolbar.barStyle = .default
+        toolbar.isTranslucent = true
+        toolbar.tintColor = .blue
+        toolbar.sizeToFit()
+        //TODO: cancel button too?
+        let flexButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(donePicker))
+        toolbar.setItems([flexButton, doneButton], animated: false)
+        toolbar.isUserInteractionEnabled = true
+
+        //TODO: initialize them
+        startTextField.text = entry.startTime.formattedTime()
+        startTimePicker = UIDatePicker()
+        startTimePicker.datePickerMode = .time
+        startTimePicker.date = entry.startTime
+        startTextField.inputView = startTimePicker
+        startTextField.inputAccessoryView = toolbar
+
+        let duration = stopTime.timeIntervalSince(entry.startTime)
+        durationTextField.text = Date.formattedDuration(duration)
+        durationTimePicker = UIDatePicker()
+        durationTimePicker.datePickerMode = .countDownTimer
+        durationTimePicker.countDownDuration = duration
+        durationTextField.inputView = durationTimePicker
+        durationTextField.inputAccessoryView = toolbar
+    }
+
+//    // MARK: Picker View
+//
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        return 1
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//        return 1
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        return nil
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//
+//    }
+
+    //TODO: hook up the value changed event so the text updates that way
+    @objc func donePicker(sender: UIBarButtonItem) {
+        if startTextField.isEditing {
+            //TODO: pull out the value
+            if let timeEntry = entry {
+                timeEntry.startTime = startTimePicker.date
+                startTextField.text = timeEntry.startTime.formattedTime()
+            }
+            startTextField.endEditing(true)
+        }
+        if durationTextField.isEditing {
+            //TODO: pull out the value
+            let duration = durationTimePicker.countDownDuration
+            if let timeEntry = entry {
+                timeEntry.stopTime = timeEntry.startTime.addingTimeInterval(duration)
+                durationTextField.text = Date.formattedDuration(duration)
+            }
+            durationTextField.endEditing(true)
+        }
+    }
+
+    // MARK: Text Field - not sure this section is needed
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+
+    }
+
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+
+        return true
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true
+    }
+
+    // MARK: Button Actions
+
     @objc func saveTapped() {
         print("saveTapped")
+        TimeEntryController.shared.saveAllEntries()
         navigationController?.popViewController(animated: true)
+        //TODO: get the main view to reload the cell w/ this entry
     }
 
     @IBAction func deleteTapped(_ sender: Any) {
